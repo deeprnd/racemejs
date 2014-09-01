94504306a3b3939e7c87a7b58f3277dbcaba38af
Victor Genin <victor.genin@live.com>
2014-09-01 10:45:32 +0300

    TravicCI integration

--- gruntfile.js
+++ gruntfile.js
@@ -5,16 +5,23 @@
             pkg: grunt.file.readJSON('package.json'),
             karma: {
                 unit_global: {
-                    configFile: 'karma.conf.js'
+                    configFile: 'karma.conf.js',
+                    autoWatch: false,
+                    singleRun: true,
+                    reporters: ['dots']
                 },
 
                 unit_requirejs: {
-                    configFile: 'karma.conf.require.js'
+                    configFile: 'karma.conf.require.js',
+                    autoWatch: false,
+                    singleRun: true,
+                    reporters: ['dots']
                 }
             },
             jasmine_node: {
                 options: {
                     forceExit: true,
+                    isVerbose: false,
                     match: '.',
                     matchall: false,
                     extensions: 'js',
